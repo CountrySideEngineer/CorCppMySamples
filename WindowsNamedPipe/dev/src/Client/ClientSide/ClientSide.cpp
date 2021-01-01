713040cdb4c7dc1c9a5540c255d0db399b7fb387
CountrySideEngineer <deep-sleeper@nifty.com>
2021-01-01 13:38:13 +0900

    Add server and client program for named pipe. -Add server program developing solution of VisualStudio using named pipe. -Add client program developing solution of VisualStudio.

---
+++ WindowsNamedPipe/dev/src/Client/ClientSide/ClientSide.cpp
@@ -0,0 +1,81 @@
+﻿// ClientSide.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ClientSide.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 唯一のアプリケーション オブジェクトです
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // MFC を初期化して、エラーの場合は結果を出力する
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: アプリケーションの動作を記述するコードをここに挿入してください。
+            wprintf(L"致命的なエラー: MFC の初期化が失敗しました\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            HANDLE hPipe = CreateFile(_T("\\\\.\\pipe\\sample_pipe"),
+                GENERIC_WRITE,
+                0, 
+                NULL, 
+                OPEN_EXISTING, 
+                FILE_ATTRIBUTE_NORMAL, 
+                NULL);
+            if (INVALID_HANDLE_VALUE == hPipe) {
+                _tprintf(_T("Can not create file, as PIPE\r\n"));
+            }
+            else {
+                _tprintf(_T("Can create file, as PIPE\r\n"));
+                int loopCount = 0;
+                int maxLoopNum = 5;
+                do {
+                    _TCHAR buffer[256];
+                    ZeroMemory(buffer, sizeof(buffer));
+                    _stprintf_s(buffer, _T("CLIENT SEND DATA = %d"), loopCount);
+                    DWORD writtenWordSize = 0;
+                    DWORD numberOfByteToWrite = _tcsclen(buffer) * sizeof(TCHAR);
+                    _tprintf(_T("Send data len : %d\r\n"), numberOfByteToWrite);
+                    BOOL writeRes = WriteFile(hPipe, buffer, numberOfByteToWrite, (LPDWORD)&writtenWordSize, NULL);
+                    if (!writeRes) {
+                        _tprintf(_T("Can not write data into file.\r\n"));
+                    }
+                    else {
+                        _tprintf(_T("Can write data into file.(data size = %d)\r\n"), writtenWordSize);
+                    }
+                    loopCount++;
+
+                    Sleep(100);
+                } while (loopCount < maxLoopNum);
+            }
+            CloseHandle(hPipe);
+            // TODO: アプリケーションの動作を記述するコードをここに挿入してください。
+        }
+    }
+    else
+    {
+        // TODO: 必要に応じてエラー コードを変更してください
+        wprintf(L"致命的なエラー: GetModuleHandle が失敗しました\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
---
+++ WindowsNamedPipe/dev/src/Server/ServerSide/ServerSide.cpp
@@ -0,0 +1,90 @@
+﻿// ServerSide.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ServerSide.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 唯一のアプリケーション オブジェクトです
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // MFC を初期化して、エラーの場合は結果を出力する
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: アプリケーションの動作を記述するコードをここに挿入してください。
+            wprintf(L"致命的なエラー: MFC の初期化が失敗しました\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            HANDLE hPipe = INVALID_HANDLE_VALUE;
+            hPipe = CreateNamedPipe(_T("\\\\.\\pipe\\sample_pipe"),
+                PIPE_ACCESS_INBOUND,
+                PIPE_TYPE_BYTE | PIPE_WAIT,
+                1,
+                0,
+                0,
+                100,
+                NULL);
+            if (INVALID_HANDLE_VALUE == hPipe) {
+                _tprintf(_T("The pipe can not open.\r\n"));
+            }
+            else {
+                _tprintf(_T("The pipe can open.\r\n"));
+            }
+            if (0 == ConnectNamedPipe(hPipe, NULL)) {
+                _tprintf(_T("Can not connect named pipe"));
+            }
+            else {
+                _tprintf(_T("Can connect named pipe"));
+            }
+
+            SHORT failedCount = 0;
+            do {
+                _TCHAR buffer[256];
+                ZeroMemory(buffer, 256);
+                DWORD readDataSize = 0;
+                BOOL readRes = ReadFile(hPipe, buffer, sizeof(buffer), (LPDWORD)&readDataSize, NULL);
+                if (!readRes) {
+                    //_tprintf(_T("Can not read data.\r\n"));
+                    failedCount++;
+                }
+                else {
+                    _tprintf(_T("Can read data : \r\n"));
+                    _tprintf(_T("Read data size : %d\r\n"), readDataSize);
+                    _tprintf(_T("Read data : %s\r\n"), buffer);
+                    failedCount = 0;
+                }
+                Sleep(10);
+            } while (failedCount < 2000);   //Max 20 second
+
+            FlushFileBuffers(hPipe);
+            DisconnectNamedPipe(hPipe);
+            CloseHandle(hPipe);
+        }
+    }
+    else
+    {
+        // TODO: 必要に応じてエラー コードを変更してください
+        wprintf(L"致命的なエラー: GetModuleHandle が失敗しました\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
