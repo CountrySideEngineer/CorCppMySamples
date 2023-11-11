58cfccf2bfd0e1aba595865631c16e833eb3d226
CountrySideEngineer <deep-sleeper@nifty.com>
2023-11-11 18:30:13 +0900

    Update codes.

--- RandomSample/RandomSample/RandomSample.cpp
+++ RandomSample/RandomSample/RandomSample.cpp
@@ -4,15 +4,14 @@
 
 #define BLOCK_NUM       (100)           //100block
 #define DATA_SIZE       (BLOCK_NUM)     //Sample data size.
+#define SAMPLING_NUM    (1000)         //Sampling count
 
-LONG    AppearTable[BLOCK_NUM] = { false };
-
-BOOL HasDisappear()
+BOOL HasDisappear(LONG appearTable[], LONG tableSize)
 {
     BOOL hasDisappear = FALSE;
 
-    for (LONG index = 0; index < DATA_SIZE; index++) {
-        if (0 == AppearTable[index]) {
+    for (LONG index = 0; index < tableSize; index++) {
+        if (0 == appearTable[index]) {
             hasDisappear = TRUE;
             break;
         }
@@ -20,30 +19,30 @@ BOOL HasDisappear()
     return hasDisappear;
 }
 
-int main()
+ULONG Loop()
 {
-    ULONG loopCount = 0;
+    LONG    appearTable[DATA_SIZE] = { 0 };
+    ULONG   loopCount = 0;
 
     srand((unsigned int)time(NULL));
 
     do {
         loopCount++;
 
-        LONG index = rand() % DATA_SIZE;
-        (AppearTable[index])++;
-        if (1 < AppearTable[index]) {
-            //_tprintf(_T("DUPLICATED : %5d\n"), index);
-            continue;
-        }
+        ULONG index = ((ULONG)rand()) % DATA_SIZE;
+        (appearTable[index])++;
+        //if (1 < AppearTable[index]) {
+        //    //_tprintf(_T("DUPLICATED : %5d\n"), index);
+        //    continue;
+        //}
 
-        if (!HasDisappear()) {
+        if (!HasDisappear(appearTable, DATA_SIZE)) {
             break;
         }
         loopCount++;
     } while (1);
 
-    _tprintf(_T("LOOP COUNT = %5d\n"), loopCount);
-
+#if 0
     for (int index = 0; index < DATA_SIZE; index++) {
         if (0 == (index % 16)) {
             if (0 != index) {
@@ -54,6 +53,30 @@ int main()
         _tprintf(_T("0x%02X "), AppearTable[index]);
     }
     _tprintf(_T("\n"));
+#endif
+    return loopCount;
+}
 
-    std::cout << "Hello World!\n";
+int main()
+{
+    ULONG   loopCounts[SAMPLING_NUM] = { 0 };
+
+    for (LONG index = 0; index < SAMPLING_NUM; index++) {
+        loopCounts[index] = Loop();
+        _tprintf(_T("%8d - %8d\n"), index, loopCounts[index]);
+
+        Sleep(1000);
+    }
+
+    //Output the number of loop in each sampling loop into csv file.
+    CONST TCHAR* csvFilePath = _T(".\\data.csv");
+    FILE* csvFile = NULL;
+    fopen_s(&csvFile, csvFilePath, "w");
+    if (NULL == csvFile) {
+        return 0;
+    }
+    for (int index = 0; index < SAMPLING_NUM; index++) {
+        _ftprintf_s(csvFile, _T("%d,"), loopCounts[index]);
+    }
+    fclose(csvFile);
 }
