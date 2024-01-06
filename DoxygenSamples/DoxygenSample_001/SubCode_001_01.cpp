e2ba2b85fbd879b5e5d662f0b47638fe8817a0ec
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-07 07:23:49 +0900

    Update SubCode_001_01.cpp. Update functions in SubCode_001_01.cpp to call function implemented in SubCode_002_01.cpp.

--- DoxygenSamples/DoxygenSample_001/SubCode_001_01.cpp
+++ DoxygenSamples/DoxygenSample_001/SubCode_001_01.cpp
@@ -9,6 +9,8 @@
 #include <Windows.h>
 #include <tchar.h>
 
+int SubFunction_002_01(int input_002_01);
+
 /**
  * @fn	SubFunction_001
  * @brief	Sample function No.001.
@@ -17,17 +19,31 @@
  * @param	input2	Input value 2.
  * @retval	This function returns the reuslt of add.
  */
-int SubFunction_001(int input1, int input2)
+int SubFunction_001_01(int input1, int input2)
 {
 	return (input1 + input2);
 }
 
 /**
- * @fn	SubFunction_002
+ * @fn	SubFunction_001_02
  * @brief	Sample function which has no argument and returns no value.
  */
-void SubFunction_002(void)
+void SubFunction_001_02(void)
 {
 	_tprintf(_T("Thsi is sample functino No. 2."));
 	return;
+}
+
+/**
+ * @fn	SubFunction_001_03
+ * @brief	Sampel function which has an argument.
+ * 
+ * @param	input_001_002	Input value to this method.
+ * @retval	
+ */
+int SubFunction_001_03(int input_001_002)
+{
+	int res = SubFunction_002_01(input_001_002);
+
+	return res;
 }
