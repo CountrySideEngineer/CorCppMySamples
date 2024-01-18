03846647f6f1794b2a15c96b0a9c89386af4829d
CountrySideEngineer <deep-sleeper@nifty.com>
2024-01-18 22:28:46 +0900

    Add SQL to get function definition from data base.

---
+++ DoxygenSamples/doxygen/GetFunctionDecSpecByAName.sql
@@ -0,0 +1,17 @@
+SELECT
+	memberdef.rowid,
+	memberdef.type AS TYPE,
+	memberdef.name AS FUNC_NAME,
+	memberdef.argsstring AS ARGUMENTS,
+	compounddef.name AS FILE_NAME,
+FROM 
+	memberdef
+LEFT JOIN compounddef
+	ON compounddef.file_id = memberdef.bodyfile_id
+WHERE
+	FUNC_NAME = 'SubFunction_001_01'
+AND
+	FILE_NAME = 'SubCode_001_01.cpp'
+LIMIT
+	1;
+	
