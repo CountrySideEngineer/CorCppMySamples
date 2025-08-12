48d8bd8336d3c13b6f1f716641a21e291ac41815
CountrySideEngineer <deep-sleeper@nifty.com>
2025-08-12 09:48:47 +0900

    Add new sample project.

---
+++ gMockSamples/src/gMockSamples/gMockSample_006/gMockSample_006.cpp
@@ -0,0 +1,67 @@
+﻿#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::_;
+using ::testing::Sequence;
+using ::testing::SetArgPointee;
+using ::testing::DoAll;
+
+// インターフェース
+class IDataProvider {
+public:
+	virtual ~IDataProvider() = default;
+	virtual bool GetValue(int* out) = 0;
+};
+
+// モッククラス
+class MockDataProvider : public IDataProvider {
+public:
+	MOCK_METHOD(bool, GetValue, (int* out), (override));
+};
+
+// テスト対象
+class Processor {
+public:
+	Processor(IDataProvider& provider) : provider_(provider) {}
+
+	int Process()
+	{
+		int value = 0;
+		int value1 = 0;
+		int value2 = 0;
+		if (provider_.GetValue(&value1)) {
+			value += (value1 * 2); // 取得値を2倍して返す
+		}
+		if (provider_.GetValue(&value2)) {
+			value += (value2 * 2); // 取得値を2倍して返す
+		}
+
+		return value;
+	}
+
+private:
+	IDataProvider& provider_;
+};
+
+// テスト
+TEST(ProcessorTest, ReturnsDoubleOfProvidedValue)
+{
+	MockDataProvider mock;
+	Sequence seq;
+
+	// GetValue が複数回呼び出された場合の動作をセット
+	// GetValue の呼び出し時、引数(out)に 1 をセットし、true を返す
+	EXPECT_CALL(mock, GetValue(_))
+		.Times(1)			// Times() → InSequence()の順番で実行すること。
+		.InSequence(seq)	// この順番が守られない場合、ビルド時にエラーになる。
+		.WillOnce(DoAll(SetArgPointee<0>(1), testing::Return(true)));
+
+	// GetValue の呼び出し時、引数(out)に 1 をセットし、true を返す
+	EXPECT_CALL(mock, GetValue(_))
+		.Times(1)
+	 	.InSequence(seq)
+		.WillOnce(DoAll(SetArgPointee<0>(2), testing::Return(true)));
+
+	Processor proc(mock);
+	EXPECT_EQ(proc.Process(), 6);
+}
