ec4bb8819f631ca6fb77ae4310df61c17c235752
CountrySideEngineer <deep-sleeper@nifty.com>
2025-08-12 09:32:34 +0900

    Add new sample project.

---
+++ gMockSamples/src/gMockSamples/gMockSample_005/gMockSample_005.cpp
@@ -0,0 +1,52 @@
+﻿#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+using ::testing::_;
+using ::testing::Return;
+using ::testing::Sequence;
+
+// インターフェース
+class ICalculator {
+public:
+	virtual ~ICalculator() = default;
+	virtual int Add(int a, int b) = 0;
+};
+
+// モッククラス
+class MockCalculator : public ICalculator {
+public:
+	MOCK_METHOD(int, Add, (int a, int b), (override));
+};
+
+// テスト対象
+class MathClient {
+public:
+	MathClient(ICalculator& calc) : calc_(calc) {}
+
+	int Run()
+	{
+		int r1 = calc_.Add(1, 2);  // 1回目
+		int r2 = calc_.Add(5, 7);  // 2回目
+		return r1 + r2;
+	}
+
+private:
+	ICalculator& calc_;
+};
+
+// テスト
+TEST(MathClientTest, MultipleCallsWithDifferentArgs)
+{
+	MockCalculator mock;
+	Sequence s; // 呼び出し順を保証するためのシーケンス
+
+	EXPECT_CALL(mock, Add(1, 2))
+		.InSequence(s)
+		.WillOnce(Return(3));
+
+	EXPECT_CALL(mock, Add(5, 7))
+		.InSequence(s)
+		.WillOnce(Return(12));
+
+	MathClient client(mock);
+	EXPECT_EQ(client.Run(), 15); // 3 + 12 = 15
+}
